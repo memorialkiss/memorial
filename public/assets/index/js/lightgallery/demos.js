b4d7f5d57b4acdab125f804661ba028c64efeb29
Rafael Nascimento <rdsnascimento@inf.ufpel.edu.br>
2019-05-18 05:47:21 -0300

    corrigido bug galeria

--- public/assets/index/js/lightgallery/demos.js
+++ public/assets/index/js/lightgallery/demos.js
@@ -2,57 +2,62 @@ $(document).ready(function () {
 
     window.prettyPrint && prettyPrint()
 
-    if ($(window).width() >= 768) {
-        $('.galeria_fotos').each(function (i, el) {
-            $(el).justifiedGallery({
+    var urlAtual = window.location.href;
+    urlAtual = urlAtual.split('/vitima')[1];
+
+    //esta na pagina principal
+    if (urlAtual == undefined) {
+        if ($(window).width() >= 768) {
+            $('.galeria_fotos').each(function (i, el) {
+                $(el).justifiedGallery({
+                    rowHeight: 120,
+                    border: 6,
+                    lastRow: 'nojustify'
+                }).on('jg.complete', function () {
+                    $animThumb.lightGallery({
+                        thumbnail: true
+                    });
+                });
+            });
+        } else {
+            $('.galeria_fotos').each(function (i, el) {
+                $(el).justifiedGallery({
+                    rowHeight: 80,
+                    border: 10,
+                    lastRow: 'nojustify'
+                }).on('jg.complete', function () {
+                    $animThumb.lightGallery({
+                        thumbnail: true
+                    });
+                });
+            });
+        }
+
+        var $animThumb = $('#galeria_fotos');
+        if ($animThumb.length) {
+            $animThumb.justifiedGallery({
                 rowHeight: 120,
-                border: 6,
-                lastRow: 'nojustify'
+                margins: 2,
+                lastRow: 'justify',
             }).on('jg.complete', function () {
                 $animThumb.lightGallery({
                     thumbnail: true
                 });
             });
-        });
-    } else {
-        $('.galeria_fotos').each(function (i, el) {
-            $(el).justifiedGallery({
-                rowHeight: 80,
-                border: 10,
-                lastRow: 'nojustify'
+        };
+    } else { //esta na pagina de vitima
+        var $animThumb = $('#galeria_fotos');
+        if ($animThumb.length) {
+            $animThumb.justifiedGallery({
+                rowHeight: 120,
+                maxRowHeight: 140,
+                margins: 2,
+                lastRow: 'justify',
             }).on('jg.complete', function () {
                 $animThumb.lightGallery({
                     thumbnail: true
                 });
             });
-        });
+        };
     }
-
-    // Animated thumbnails
-    var $animThumb = $('#galeria_fotos');
-    if ($animThumb.length) {
-        $animThumb.justifiedGallery({
-            rowHeight: 120,
-            margins: 2,
-            lastRow: 'justify',
-        }).on('jg.complete', function () {
-            $animThumb.lightGallery({
-                thumbnail: true
-            });
-        });
-    };
-
-    var $animThumb = $('#galeria-vitima');
-    if ($animThumb.length) {
-        $animThumb.justifiedGallery({
-            rowHeight: 120,
-            maxRowHeight: 140,
-            margins: 2,
-            lastRow: 'justify',
-        }).on('jg.complete', function () {
-            $animThumb.lightGallery({
-                thumbnail: true
-            });
-        });
-    };
 });
